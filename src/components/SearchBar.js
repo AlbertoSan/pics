86fedcfdc5e2b26a8f30ef29902625c82f0e7c12
Alberto Sánchez <albertoensan@gmail.com>
2020-10-20 01:22:29 +0200

    add handler

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+
+    onInputClick() {
+        console.log('Input was clicked');
+    }
+
+    //el handler tambien puede usarse como arrow function
+    //en el input se podria llamar de la siguiente forma: onChange={(e) => console.log(e.target.value)}
     render() {
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
-                        <label>Image Search</label>
-                        <input type="text" />
+                        <label>Image Search</label>                        
+                        <input type="text" onClick={this.onInputClick} onChange={this.onInputChange}/>
                     </div>                    
                 </form>
             </div>
