457c5a383878d4b85f52e97599404c0b918a8e7f
Alberto Sánchez <albertoensan@gmail.com>
2020-10-20 03:06:16 +0200

    arrow function para solucionar error undefined

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+    constructor() {
+        super();
+
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+    }
+
     onInputChange(event) {
         console.log(event.target.value);
     }
@@ -9,15 +15,27 @@ class SearchBar extends React.Component {
         console.log('Input was clicked');
     }
 
+    state = { term: '' };
+
+    onFormSubmit(event) {
+        event.preventDefault();
+
+        console.log(this.state.term);
+    }
+
     //el handler tambien puede usarse como arrow function
     //en el input se podria llamar de la siguiente forma: onChange={(e) => console.log(e.target.value)}
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image Search</label>                        
-                        <input type="text" onClick={this.onInputClick} onChange={this.onInputChange}/>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onClick={this.onInputClick}
+                            onChange={(e) => this.setState({ term: e.target.value.toUpperCase() })} />
                     </div>                    
                 </form>
             </div>
